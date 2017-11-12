b7790806641fd23bcf332fbe141fc1c71692ba57
Ethan Brown <ethanbrown@Corkys-MacBook-Pro.local>
2017-11-11 21:57:10 -0500

    i did a thing

---
+++ src/pages/totallynotlogin/Index.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-layout>
+    <v-card contextual-style="dark">
+       <div slot="body">Hello, world! </div>
+    </v-card>
+  </v-layout>
+</template>
+
+<script>
+  /* ============
+   * Login Index Page
+   * ============
+   *
+   * Page where the user can login.
+   */
+
+  import VLayout from '@/layouts/Minimal';
+  import VCard from '@/components/Card';
+
+  export default {
+    /**
+     * The name of the page.
+     */
+    name: 'totallynotlogin-index',
+
+    /**
+     * The data that can be used by the page.
+     *
+     * @returns {Object} The view-model data.
+     */
+    data() {
+      return {
+      };
+    },
+
+    /**
+     * The methods the page can use.
+     */
+    methods: {
+    },
+
+    /**
+     * The components the page can use.
+     */
+    components: {
+      VLayout,
+      VCard,
+    },
+  };
+</script>
--- src/routes/index.js
+++ src/routes/index.js
@@ -6,49 +6,13 @@
  */
 
 export default [
-  // Home
+  // Login (but not)
   {
-    path: '/home',
-    name: 'home.index',
-    component: () => import('@/pages/Home/Index'),
+    path: '/totallynotlogin',
+    name: 'totallynotlogin.index',
+    component: () => import('@/pages/totallynotlogin/Index'),
 
-    // If the user needs to be authenticated to view this page
-    meta: {
-      auth: true,
-    },
-  },
-
-  // Account
-  {
-    path: '/account',
-    name: 'account.index',
-    component: () => import('@/pages/Account/Index'),
-
-    // If the user needs to be authenticated to view this page.
-    meta: {
-      auth: true,
-    },
-  },
-
-  // Login
-  {
-    path: '/login',
-    name: 'login.index',
-    component: () => import('@/pages/Login/Index'),
-
-    // If the user needs to be a guest to view this page.
-    meta: {
-      guest: true,
-    },
-  },
-
-  // Register
-  {
-    path: '/register',
-    name: 'register.index',
-    component: () => import('@/pages/Register/Index.vue'),
-
-    // If the user needs to be a guest to view this page.
+    // Definitely not if the user needs to be a guest to view this page.
     meta: {
       guest: true,
     },
